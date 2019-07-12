9b508333ba92b56d67fb966f40538db3143574e2
BetDex <major058@umn.edu>
2019-07-12 02:43:27 -0500

    iniital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import Disqus from "disqus-react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css"
+
+export default class extends Component {
+  render() {
+    const disqusShortname = "your-site-shortname" //found in your Disqus.com dashboard
+    const disqusConfig = {
+      url: "http://localhost:3000", //this.props.pageUrl
+      identifier: "article-id", //this.props.uniqueId
+      title: "Title of Your Article" //this.props.title
+    }
+
+    return (
+      <div className="article-container">
+
+        <h1>Article Title</h1>
+
+        <p>Article content.</p>
+
+        <Disqus.DiscussionEmbed
+          shortname={disqusShortname}
+          config={disqusConfig}
+        />
+      </div>
+    )
+  }
 }
-
-export default App;
